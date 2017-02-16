20205c32fb5d8e407b2c6b30d6188b2ef2d5190b
Viames Marino <me@viames.it>
2017-02-16 13:05:19 +0100

    Base file struct
    Created a base file structure for the example project. This is still
    missing the database struct to create base tables.

---
+++ vendor/vms/index.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @version	$Id$
+ * @author	Viames Marino
+ * @package	VMS
+ */
+
+// interfaces declarations
+require 'interfaces.php';
+
+require 'Application.php';
+require 'Breadcrumb.php';
+require 'Controller.php';
+require 'Database.php';
+require 'DataMapper.php';
+require 'Form.php';
+require 'Input.php';
+require 'Logger.php';
+require 'Menu.php';
+require 'Model.php';
+require 'Options.php';
+require 'Pagination.php';
+require 'Plugin.php';
+require 'Router.php';
+require 'Translator.php';
+require 'Upload.php';
+require 'Utilities.php';
+require 'View.php';
+require 'Widget.php';
+
+// data mapper children
+require 'Acl.php';
+require 'ErrorLog.php';
+require 'Group.php';
+require 'Language.php';
+require 'Module.php';
+require 'Rule.php';
+require 'Session.php';
+require 'Template.php';
+require 'User.php';
