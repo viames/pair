8df3555e90a7f534d2e54bf98b115298fc8662cd
Viames Marino <me@viames.it>
2021-06-23 17:19:46 +0200

    Update Oauth2Token.php

--- src/Oauth2/Oauth2Token.php
+++ src/Oauth2/Oauth2Token.php
@@ -3,13 +3,13 @@
 namespace Pair;
 
 class Oauth2Token extends ActiveRecord {
-	
+
 	/**
 	 * This property maps “id” column.
 	 * @var int
 	 */
 	protected $id;
-	
+
 	/**
 	 * This property maps “client_id” column.
 	 * @var string
@@ -45,7 +45,7 @@ class Oauth2Token extends ActiveRecord {
 	 * @var int
 	 */
 	const LIFETIME = 3600;
-	
+
 	/**
 	 * Name of primary key db field.
 	 * @var string|array
@@ -63,51 +63,78 @@ class Oauth2Token extends ActiveRecord {
 
 	}
 
+	private static function getAuthorizationHeader(): ?string {
+
+		$headers = NULL;
+
+		if (isset($_SERVER['Authorization'])) {
+
+			$headers = trim($_SERVER["Authorization"]);
+
+		} else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+
+			$headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+
+		} else if (function_exists('apache_request_headers')) {
+
+			$requestHeaders = apache_request_headers();
+
+			// Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+			$requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+
+			//print_r($requestHeaders);
+
+			if (isset($requestHeaders['Authorization'])) {
+				$headers = trim($requestHeaders['Authorization']);
+			}
+
+		}
+
+		return $headers;
+
+	}
+
 	/**
-     * Get access token from header.
-     */
-    public static function readBearerToken(): ?string {
+	 * Get access token from header.
+	 *
+	 * @return string|NULL
+	 */
+	public static function readBearerToken(): ?string {
 
-        $headers = NULL;
-        
-        if (isset($_SERVER['Authorization'])) {
-        
-            $headers = trim($_SERVER["Authorization"]);
-        
-        } else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
-        
-            $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
-        
-        } else if (function_exists('apache_request_headers')) {
-        
-            $requestHeaders = apache_request_headers();
-            
-            // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
-            $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
-            
-            //print_r($requestHeaders);
-            
-            if (isset($requestHeaders['Authorization'])) {
-                $headers = trim($requestHeaders['Authorization']);
-            }
+		$headers = self::getAuthorizationHeader();
 
-        }
+		if (!empty($headers) and preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+			return $matches[1];
+		}
 
-        // HEADER: Get the access token from the header
-        if (!empty($headers)) {
-            if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
-                return $matches[1];
-            }
-        }
+		return NULL;
 
-        return NULL;
+	}
 
-    }
+	/**
+	 * Read client_id and client_secret from header authorization.
+	 *
+	 * @return \stdClass|NULL
+	 */
+	public static function readBasicAuth(): ?\stdClass {
+
+		$headers = self::getAuthorizationHeader();
+
+		if (!empty($headers) and preg_match('/Basic\s(\S+)/', $headers, $matches)) {
+			$parts = base64_decode($matches[1]);
+			$client = new \stdClass();
+			list($client->id, $client->secret) = explode(':', $parts, 2);
+			return $client;
+		}
+
+		return NULL;
+
+	}
 
 	/**
 	 * Verify that the past token exists and has a compatible date and creates a past date for the number of seconds in duration
-	 * @param string $bearerToken 
-	 * @return bool 
+	 * @param string $bearerToken
+	 * @return bool
 	 */
 	public static function validate(string $bearerToken): bool {
 
@@ -129,9 +156,9 @@ class Oauth2Token extends ActiveRecord {
 
 	/**
 	 * The request could not be understood by the server due to malformed syntax. The client
-	 * SHOULD NOT repeat the request without modifications. 
-	 * @param string $detail 
-	 * @return void 
+	 * SHOULD NOT repeat the request without modifications.
+	 * @param string $detail
+	 * @return void
 	 */
 	public static function badRequest(string $detail): void {
 
@@ -145,8 +172,8 @@ class Oauth2Token extends ActiveRecord {
 	 * resource. The client MAY repeat the request with a suitable Authorization header field
 	 * (section 14.8). If the request already included Authorization credentials, then the 401
 	 * response indicates that authorization has been refused for those credentials.
-	 * @param string $detail 
-	 * @return void 
+	 * @param string $detail
+	 * @return void
 	 */
 	public static function unauthorized(string $detail): void {
 
@@ -161,22 +188,22 @@ class Oauth2Token extends ActiveRecord {
 	 * describe the reason for the refusal in the entity. If the server does not wish to make
 	 * this information available to the client, the status code 404 (Not Found) can be used
 	 * instead.
-	 * @param string $detail 
-	 * @return void 
+	 * @param string $detail
+	 * @return void
 	 */
 	public static function forbidden(string $detail): void {
 
 		self::sendRfc2616Response('#sec10.4.4','Forbidden','403', $detail);
 
-	}	
+	}
 
 	/**
 	 * Send a JSON object in HTTP response.
 	 * @see https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
-	 * @param string $type 
-	 * @param string $title 
-	 * @param string $status 
-	 * @param null|string $detail 
+	 * @param string $type
+	 * @param string $title
+	 * @param string $status
+	 * @param null|string $detail
 	 * @return void
 	 */
 	private static function sendRfc2616Response(string $type, string $title, string $status, ?string $detail=NULL): void {
